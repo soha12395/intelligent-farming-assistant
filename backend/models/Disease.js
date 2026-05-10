e2b604503ef94be1825b767db594ba8d81faef4a
soha12395 <souhahalawi@gmail.com>
2026-05-10 22:26:05 +0300

    fix code

--- backend/models/Disease.js
+++ backend/models/Disease.js
@@ -5,6 +5,16 @@ const Disease = {
     db.query('INSERT INTO disease_detection SET ?', data, cb);
   },
 
+  getDetectionsByProfile: (profile_id, cb) => {
+    db.query(`
+      SELECT dd.*, di.disease_name, di.plant_name, di.treatment, di.urgency
+      FROM disease_detection dd
+      LEFT JOIN disease_info di ON dd.disease_id = di.disease_id
+      WHERE dd.profile_id = ?
+      ORDER BY dd.detected_at DESC
+    `, [profile_id], cb);
+  },
+
   getDetectionsByFarmer: (profile_id, cb) => {
     db.query(`
       SELECT dd.*, di.disease_name, di.plant_name, di.treatment, di.urgency
