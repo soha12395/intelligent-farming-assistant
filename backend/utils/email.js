ee29db34274d06d8b017862e6322795a52c8c6d3
soha12395 <souhahalawi@gmail.com>
2026-05-10 17:32:20 +0300

    change to api key

--- backend/utils/email.js
+++ backend/utils/email.js
@@ -28,8 +28,8 @@ const sendVerificationCode = (email, code) => {
   };
 const transporter = nodemailer.createTransport({
   host: 'smtp-relay.brevo.com',
-  port: 587,
-  secure: false,
+  port: 465,
+  secure: true,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS
