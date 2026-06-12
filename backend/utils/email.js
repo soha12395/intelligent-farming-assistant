9949b7ee5cdb4cb1f0fcf27802e81a02f226b3aa
soha12395 <souhahalawi@gmail.com>
2026-06-12 19:00:21 +0300

    update verification code

--- backend/utils/email.js
+++ backend/utils/email.js
@@ -3,8 +3,8 @@ require("dotenv").config();
 
 const transporter = nodemailer.createTransport({
   host: 'smtp.gmail.com',
-  port: 465,
-  secure: true,
+  port: 587,
+  secure: false,
   family: 4,
   auth: {
     user: process.env.EMAIL_USER,
